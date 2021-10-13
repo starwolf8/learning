2c678c03faab28cd3e7d040029f9dc416bdbe451
starwolf8 <18685018+starwolf8@users.noreply.github.com>
2021-10-13 15:53:55 -0700

    allow logging for docker containers

--- docker-golang/main.go
+++ docker-golang/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"main/util"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 func init() {
+	setUpLogging()
+
 	_, err := util.LoadConfig(".")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
@@ -26,4 +29,14 @@ func main() {
 	router.Run(viper.GetString("SERVER_ADDRESS") + ":" +
 		viper.GetString("APP_PORT"))
 
+}
+
+func setUpLogging() {
+	file, err := os.OpenFile(
+		"logs/logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(file)
 }
