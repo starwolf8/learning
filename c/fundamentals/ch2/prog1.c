71adaa74201b9d9bf52c1e3addde1d204d1d9f86
starwolf8 <18685018+starwolf8@users.noreply.github.com>
2021-11-20 11:45:18 -0800

    more c practice

--- c/fundamentals/ch2/prog1.c
+++ c/fundamentals/ch2/prog1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
     printf ("hello world!\n");
+    int sum;
+
+    sum = 50 + 25;
+
+    printf ("The sum of 50 and 25 is %i\n", sum);
 
     return 0;
 }
--- c/fundamentals/readme.txt
+++ c/fundamentals/readme.txt
@@ -1,3 +1,5 @@
 compiling the program: `gcc prog1.c`
 
 running the program `./a.out`
+
+to specify a different name `gcc prog1.c -o prog1`
